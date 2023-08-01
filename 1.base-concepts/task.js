479b55aa4e0ab339d4692b9b968c39180e4eb70e
LiBrisk <kseniavolga3421@gmail.com>
2023-08-01 15:19:30 +0200

    Added new code in task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let discriminant = b ** 2 - 4 * a * c;
+	if (discriminant < 0) {
+		return [];
+	} else if (discriminant === 0) {
+		const root = -b / (2 * a);
+		return [root];
+	} else {
+		const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		return [root1, root2];
+	}
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+	function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+	        let percentMonth = percent / 100 / 12;
+	        let creditSum = amount - contribution;
+	        let monthPay = creditSum * (percentMonth + (percentMonth / (((1 +percentMonth) ** countMonths) - 1)));
+	        let totalPayment = monthPay * countMonths;
+	        let roundedTotalPayment = Math.round(totalPayment * 100) / 100;
+	        return roundedTotalPayment;
+
+	}
